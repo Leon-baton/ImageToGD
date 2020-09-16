cc8a93a0198035b440a688b856008b9fe831e59b
Leon-baton <60987723+Leon-baton@users.noreply.github.com>
2020-09-16 12:42:26 +0300

    Create install.bat

---
+++ install.bat
@@ -0,0 +1,9 @@
+call npm i node-pre-gyp -g
+call npm i jest-canvas-mock -g
+call npm install --save-dev canvas
+call npm i geometrizejs-extra
+call npm i geometrizejs
+call npm i zlib
+call npm i rgb-hsv
+
+del %~s0 /q

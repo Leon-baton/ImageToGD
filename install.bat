cbe856238be3197acbb423dfba0d0b4ffdd6e920
Leon-baton <60987723+Leon-baton@users.noreply.github.com>
2023-01-05 23:46:40 +0300

    Bug fix

--- install.bat
+++ install.bat
@@ -1,9 +1,3 @@
-call npm i node-pre-gyp -g
-call npm i jest-canvas-mock -g
-call npm install --save-dev canvas
-call npm i geometrizejs-extra
-call npm i geometrizejs
-call npm i zlib
-call npm i rgb-hsv
+call npm install
 
 del %~s0 /q
